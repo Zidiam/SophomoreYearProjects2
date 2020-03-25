5237ba718208958d08551a4829d370e9a1af179d
melni <melni@DESKTOP-6INVJPV>
2020-03-25 05:10:15 -0400

    first

---
+++ 91-divoc/src/main/java/divocMain.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+/*
+ * divocMain,java -- A fun game
+ * By: Jason Melnik
+ * Date: 3/25/2020
+ */
+public class divocMain {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("91-divoc");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(new divocMainPanel());
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(true);
+	}
+}
